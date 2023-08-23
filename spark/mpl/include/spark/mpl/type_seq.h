f3148454f87cb1aa108fd3ad1df0dd5f76d53c4f
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2023-08-23 20:29:50 +0200

    spark/mpl: [Enh] Add typelist reverse, filter, find, convert (#28)

--- spark/mpl/include/spark/mpl/type_seq.h
+++ spark/mpl/include/spark/mpl/type_seq.h
@@ -60,6 +60,25 @@ namespace spark::mpl::type_seq
     template <typename T, typename... Ts>
     inline static constexpr std::size_t index_of_v = index_of<T, Ts...>::value;
 
+    // find
+    template <typename T, typename... Ts>
+    struct find {};
+
+    template <template<typename...> typename C, typename T, typename Head, typename... Ts>
+    struct find<T, C<Head, Ts...>>
+    {
+        inline static constexpr std::size_t value = find<T, C<Ts...>>::value + 1;
+    };
+
+    template <template<typename...> typename C, typename T, typename... Ts>
+    struct find<T, C<T, Ts...>>
+    {
+        inline static constexpr std::size_t value = 0;
+    };
+
+    template <typename T, typename... Ts>
+    inline static constexpr std::size_t find_v = find<T, Ts...>::value;
+
     // front
     template <typename... Ts>
     struct front { };
@@ -287,4 +306,53 @@ namespace spark::mpl::type_seq
 
     template <std::size_t N, typename T, typename... Ts>
     using replace_at_t = typename replace_at<N, T, Ts...>::type;
+
+    // reverse
+    template <typename... Ts>
+    struct reverse {};
+
+    template <template<typename...> typename C, typename... TypesToAdd, typename Head, typename... Ts>
+    struct reverse<C<Head, Ts...>, TypesToAdd...>
+    {
+        using type = typename reverse<C<Ts...>, Head, TypesToAdd...>::type;
+    };
+
+    template <template<typename...> typename C, typename... TypesToAdd>
+    struct reverse<C<>, TypesToAdd...>
+    {
+        using type = C<TypesToAdd...>;
+    };
+
+    template <typename... Ts>
+    using reverse_t = typename reverse<Ts...>::type;
+
+    // filter
+    template <template <typename> typename Predicate, typename... Ts>
+    struct filter {};
+
+    template <template<typename...> typename C, template <typename> typename Predicate, typename Head, typename... Ts>
+    struct filter<Predicate, C<Head, Ts...>>
+    {
+        using type = std::conditional_t<Predicate<Head>::value, typename push_front<Head, typename filter<Predicate, C<Ts...>>::type>::type, typename filter<
+                                            Predicate, C<Ts...>>::type>;
+    };
+
+    template <template<typename...> typename C, template <typename> typename Predicate>
+    struct filter<Predicate, C<>>
+    {
+        using type = C<>;
+    };
+
+    template <template <typename> typename Predicate, typename... Ts>
+    using filter_t = typename filter<Predicate, Ts...>::type;
+
+    // convert
+    template <template <typename...> typename T, typename... Ts>
+    struct convert
+    {
+        using type = T<Ts...>;
+    };
+
+    template <template <typename...> typename T, typename... Ts>
+    using convert_t = typename convert<T, Ts...>::type;
 }
