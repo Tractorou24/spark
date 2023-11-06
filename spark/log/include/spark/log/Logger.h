40729f69f64b490d553d74c3ea3d0a6d295893d3
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2023-11-06 16:00:57 +0100

    spark/log: [Enh] Add new logger implementation (#30)

--- spark/log/include/spark/log/Logger.h
+++ spark/log/include/spark/log/Logger.h
@@ -1,3 +1,90 @@
 #pragma once
 
 #include "spark/log/Export.h"
+#include "spark/log/Level.h"
+
+#include <format>
+#include <string_view>
+
+namespace spark::log
+{
+    /**
+     * \brief Logs a message with the trace log level
+     * \details Logs into a file (spark.log) in the executable launch directory and in the console (white color)
+     * \param level The \ref spark::log::Level for the message
+     * \param message A \ref std::string containing the message to log
+     */
+    SPARK_LOG_EXPORT void log(Level level, const std::string& message);
+
+    /**
+     * \brief Logs a message with the trace log level
+     * \details Logs into a file (spark.log) in the executable launch directory and in the console (white color)
+     * \param message A \ref std::string_view containing the message to log
+     * \param args The arguments to format the message with
+     */
+    template <typename... Args>
+    void trace(std::string_view message, Args&&... args)
+    {
+        log(Level::Trace, std::vformat(message, std::make_format_args(args...)));
+    }
+
+    /**
+     * \brief Logs a message with the debug log level
+     * \details Logs into a file (spark.log) in the executable launch directory and in the console (blue color)
+     * \param message A \ref std::string_view containing the message to log
+     * \param args The arguments to format the message with
+     */
+    template <typename... Args>
+    void debug(std::string_view message, Args&&... args)
+    {
+        log(Level::Debug, std::vformat(message, std::make_format_args(args...)));
+    }
+
+    /**
+     * \brief Logs a message with the info log level
+     * \details Logs into a file (spark.log) in the executable launch directory and in the console (green color)
+     * \param message A \ref std::string_view containing the message to log
+     * \param args The arguments to format the message with
+     */
+    template <typename... Args>
+    void info(std::string_view message, Args&&... args)
+    {
+        log(Level::Info, std::vformat(message, std::make_format_args(args...)));
+    }
+
+    /**
+     * \brief Logs a message with the warning log level
+     * \details Logs into a file (spark.log) in the executable launch directory and in the console (yellow color)
+     * \param message A \ref std::string_view containing the message to log
+     * \param args The arguments to format the message with
+     */
+    template <typename... Args>
+    void warning(std::string_view message, Args&&... args)
+    {
+        log(Level::Warning, std::vformat(message, std::make_format_args(args...)));
+    }
+
+    /**
+     * \brief Logs a message with the error log level
+     * \details Logs into a file (spark.log) in the executable launch directory and in the console (red color)
+     * \param message A \ref std::string_view containing the message to log
+     * \param args The arguments to format the message with
+     */
+    template <typename... Args>
+    void error(std::string_view message, Args&&... args)
+    {
+        log(Level::Error, std::vformat(message, std::make_format_args(args...)));
+    }
+
+    /**
+     * \brief Logs a message with the critical log level
+     * \details Logs into a file (spark.log) in the executable launch directory and in the console (white color on red background)
+     * \param message A \ref std::string_view containing the message to log
+     * \param args The arguments to format the message with
+     */
+    template <typename... Args>
+    void critical(std::string_view message, Args&&... args)
+    {
+        log(Level::Critical, std::vformat(message, std::make_format_args(args...)));
+    }
+}
--- spark/log/src/Logger.cpp
+++ spark/log/src/Logger.cpp
@@ -1,1 +1,29 @@
 #include "spark/log/Logger.h"
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace spark::log
+{
+    void log(Level level, const std::string& message)
+    {
+        static auto logger = []() -> std::shared_ptr<spdlog::logger>
+        {
+            auto color_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+            color_sink->set_pattern("%^[%T] [%l] %n: %v%$");
+
+            auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("spark.log", true);
+            file_sink->set_pattern("[%T] [%l] %n: %v");
+
+            auto core_logger = std::make_shared<spdlog::logger>("SPARK", spdlog::sinks_init_list{color_sink, file_sink});
+            spdlog::register_logger(core_logger);
+            core_logger->set_level(spdlog::level::trace);
+            core_logger->flush_on(spdlog::level::trace);
+
+            return core_logger;
+        }();
+
+        logger->log(static_cast<spdlog::level::level_enum>(level), message);
+    }
+}
