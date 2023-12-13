663822814dfe37185b0b6c2e81ebc9d7e590da4a
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2023-12-13 21:58:21 +0100

    spark/base: [Enh] Add helper macro to define bitwize operators on enum classes

--- spark/base/include/spark/base/Macros.h
+++ spark/base/include/spark/base/Macros.h
@@ -89,6 +89,16 @@
  */
 #define BIT(X) (1 << X)
 
+/**
+ * \brief Defines the bitwise operators for an enum class. (operator|, operator|=, operator&, operator&=)
+ * \param T The enum class type.
+ */
+#define SPARK_DEFINE_ENUM_FLAGS(T)                                                                                                                                                                \
+    inline T operator| (const T lhs, const T rhs) { using _base_t = std::underlying_type_t<T>; return static_cast<T>(static_cast<_base_t>(lhs) | static_cast<_base_t>(rhs)); }  \
+    inline T& operator|= (T& lhs, const T& rhs) { lhs = lhs | rhs; return lhs; }                                                                                                \
+    inline T operator& (const T lhs, const T rhs) { using _base_t = std::underlying_type_t<T>; return static_cast<T>(static_cast<_base_t>(lhs) & static_cast<_base_t>(rhs)); }  \
+    inline T& operator&= (T& lhs, const T& rhs) { lhs = lhs & rhs; return lhs; }
+
 /*
  * Define assertions macros
  * TODO: Add support for custom assertion handlers
