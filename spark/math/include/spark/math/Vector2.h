41548fbd7436314666fe1426bdeae369419eec33
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2023-12-17 19:34:06 +0100

    spark/math: [Clean] Clean and document Vector2<T> class (#56)
    This removes unneeded operator+, as well as angle() and distance()
    methods.
    Defaults initializes values at construction to avoid default
    vector2<int> components being an undefined number.

--- spark/math/include/spark/math/Vector2.h
+++ spark/math/include/spark/math/Vector2.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <utility>
-
 namespace spark::math
 {
     /**
@@ -12,8 +10,14 @@ namespace spark::math
     class Vector2 final
     {
     public:
+        /// \brief Initializes a new \ref Vector2 with all components to their default value.
         constexpr Vector2() = default;
 
+        /**
+         * \brief Initializes a new \ref Vector2 with components set to the given values.
+         * \param x The value of the x component.
+         * \param y The value of the y component.
+         */
         constexpr Vector2(T x, T y) noexcept
             : x(std::move(x)), y(std::move(y)) {}
 
@@ -24,42 +28,114 @@ namespace spark::math
         constexpr Vector2& operator=(const Vector2& other) = default;
         constexpr Vector2& operator=(Vector2&& other) noexcept = default;
 
-        // Comparison operators
-        constexpr auto operator<=>(const Vector2& other) const noexcept = default;
+        /**
+         * \brief Compares two \ref Vector2 for equality.
+         * \param other A \ref Vector2 to compare with.
+         * \return `true` if this is equal to \p other, `false` otherwise.
+         */
+        constexpr bool operator==(const Vector2& other) const noexcept;
 
-        // Arithmetic operators
+        /**
+         * \brief Compares two \ref Vector2 for inequality.
+         * \param other A \ref Vector2 to compare with.
+         * \return `true` if this is equal to \p other, `false` otherwise.
+         */
+        constexpr bool operator!=(const Vector2& other) const noexcept;
+
+        /**
+         * \brief Adds two \ref Vector2.
+         * \param other The \ref Vector2 to add.
+         * \return A \ref Vector2 resulting of the sum of the two vectors.
+         */
         constexpr Vector2 operator+(const Vector2& other) const noexcept;
+
+        /**
+         * \brief Subtracts two \ref Vector2.
+         * \param other The \ref Vector2 to subtract.
+         * \return A \ref Vector2 resulting of the subtraction of the two vectors.
+         */
         constexpr Vector2 operator-(const Vector2& other) const noexcept;
+
+        /**
+         * \brief Multiplies two \ref Vector2.
+         * \param other The \ref Vector2 to multiply with.
+         * \return A \ref Vector2 resulting of the multiplication of the two vectors.
+         */
         constexpr Vector2 operator*(const Vector2& other) const noexcept;
+
+        /**
+         * \brief Divides two \ref Vector2.
+         * \param other The \ref Vector2 to divide with.
+         * \return A \ref Vector2 resulting of the division of the two vectors.
+         */
         constexpr Vector2 operator/(const Vector2& other) const noexcept;
+
+        /**
+         * \brief Multiplies a \ref Vector2 by a value of type T
+         * \param value The value of type T to multiply the vector with
+         * \return A \ref Vector2 resulting of the multiplication with \p value.
+         */
         constexpr Vector2 operator*(const T& value) const noexcept;
+
+        /**
+         * \brief Divides a \ref Vector2 by a value of type T
+         * \param value The value of type T to divide the vector with
+         * \return A \ref Vector2 resulting of the division with \p value.
+         */
         constexpr Vector2 operator/(const T& value) const noexcept;
 
-        // Compound assignment operators
+        /**
+         * \brief Adds the value of \p other to the current vector.
+         * \param other The \ref Vector2 to take values to add.
+         * \return A reference to the modified \ref Vector2.
+         */
         constexpr Vector2& operator+=(const Vector2& other) noexcept;
+
+        /**
+         * \brief Subtracts the value of \p other to the current vector.
+         * \param other The \ref Vector2 to take values to subtract.
+         * \return A reference to the modified \ref Vector2.
+         */
         constexpr Vector2& operator-=(const Vector2& other) noexcept;
+
+        /**
+         * \brief Multiplies the value of \p other to the current vector.
+         * \param other The \ref Vector2 to take values to multiply with.
+         * \return A reference to the modified \ref Vector2.
+         */
         constexpr Vector2& operator*=(const Vector2& other) noexcept;
+
+        /**
+         * \brief Divides the value of \p other from the current vector.
+         * \param value The value to divide each component with.
+         * \return A reference to the modified \ref Vector2.
+         */
         constexpr Vector2& operator/=(const T& value) noexcept;
 
-        // Unary operators
+        /**
+         * \brief Negates all components of the current \ref Vector2.
+         * \return A \ref Vector2 with all components negated.
+         */
         constexpr Vector2 operator-() const noexcept;
-        constexpr Vector2 operator+() const noexcept;
 
-        // Misc
-        friend constexpr void swap(Vector2& lhs, Vector2& rhs) noexcept
-        {
-            std::swap(lhs.x, rhs.x);
-            std::swap(lhs.y, rhs.y);
-        }
+        /**
+         * \brief Swaps two \ref Vector2.
+         * \param lhs The first vector.
+         * \param rhs The second vector.
+         */
+        friend constexpr void swap(Vector2& lhs, Vector2& rhs) noexcept;
 
-        [[nodiscard]] constexpr T distance(const Vector2& other) const noexcept;
-        [[nodiscard]] constexpr T angle(const Vector2& other) const noexcept;
-
+        /**
+         * \brief Casts all components of the \ref Vector2 to the type To.
+         * \tparam To Type of the components of the new \ref Vector2.
+         * \return A \ref Vector2 of type To.
+         */
         template <typename To>
         [[nodiscard]] constexpr Vector2<To> castTo() const noexcept;
 
     public:
-        T x, y;
+        T x {};
+        T y {};
     };
 }
 
