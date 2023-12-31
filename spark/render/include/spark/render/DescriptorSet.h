36a28d403f774f2aba039ecef4372b582d38fc3e
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-01-01 00:16:56 +0100

    spark/render: [Enh] Add DescriptorSet class to use custom types (#27)

--- spark/render/include/spark/render/DescriptorSet.h
+++ spark/render/include/spark/render/DescriptorSet.h
@@ -150,6 +150,74 @@ namespace spark::render
         virtual void genericAttach(unsigned int binding, const IImage& image) const = 0;
         /// @}
     };
+
+    /**
+     * \brief Defines a set of descriptors.
+     * \tparam BufferType The type of the buffer interface. (inherits from \ref IBuffer)
+     * \tparam ImageType The type of the image interface. (inherits from \ref IImage)
+     * \tparam SamplerType The type of the sampler interface. (inherits from \ref ISampler)
+     *
+     * Descriptors can be grouped into multiple descriptor sets. It is generally a good practice to group descriptors based on the frequency of the updates they receive.
+     * For example, it typically makes sense to store the camera buffer in a descriptor set, since it only needs to be updated once per frame for each camera, whilst the object
+     * or material data should be stored in separate descriptor sets, that are possibly updated before each draw call.
+     *
+     * From a shader perspective, a descriptor set is identified by a `set` (GLSL) or `space` (HLSL), whilst a descriptor is addressed by a `binding` (GLSL) or 
+     * `register` (HLSL). Descriptor sets are read from GPU-visible memory, depending on how they are bound during the current draw call.
+     * 
+     * From a CPU perspective, think of a descriptor set as an array of pointers to different buffers (i.e. descriptors) for the shader. A descriptor can be bound to a set by 
+     * calling \ref IDescriptorSet::update(). Note that this does not automatically ensure, that the buffer memory is visible for the GPU. Instead, a buffer may also 
+     * require a transfer into GPU visible memory, depending on the \ref BufferUsage. However, as long as a descriptor within a set is mapped to a buffer, modifying 
+     * this buffer also reflects the change to the shader, without requiring to update the descriptor, similarly to how modifying the object behind a pointer does not require 
+     * the pointer to change.
+     * 
+     * Note, that there might be multiple descriptor set instances of the same \ref DescriptorSetLayout, pointing to different \ref IBuffer instances, depending on the number
+     * of frames in flight. Since multiple frames can be computed concurrently, it is important to properly synchronize descriptor set updates.
+     *
+     * Generally, there are three strategies to choose from, that you can implement or mix in custom flavors, depending on your use case:
+     *     - Naive: The naive approach most closely matches earlier graphics API concepts. Create one buffer per descriptor and synchronize frames. This basically means that each 
+     *         back buffer swap is synchronized to wait for the graphics pipeline. This way, writing to a buffer ensures, that it is only read within the frame of reference and 
+     *         modifying it does not interfere with other frames. This strategy is memory efficient, but may cause the GPU to stall. It may, however be a valid strategy, for 
+     *         data that is only written once or very infrequently.
+     *     - Array of Buffers: The helper methods for creating and updating constant buffers are able to create buffer arrays. Those arrays can be used to create a buffer for each frame in 
+     *         flight. When binding a buffer to a descriptor, it is possible to bind only one element of the array. This way, each frame has its own buffer and does not 
+     *         interfere with other buffer writes.
+     *     - Ring-Buffer: The most efficient (yet not always applicable) approach involves creating one large buffer array, that is bound to multiple descriptor sets. This ensures that
+     *         the buffer memory stays contiguous and does not get fragmented. However, this requires to know upfront, how many buffers are required for each descriptor, which
+     *         might not always be possible. Thus another flavor of using this technique involves a creating a large enough descriptor array and updating the descriptor
+     *         set with an increasing array element for each object as a ring-buffer. As long as there are enough elements in the buffer, so that no second update interferes 
+     *         with a buffer write in an earlier frame, this method provides the most efficient approach. However, it may be hard or impossible to determine the ideal size of 
+     *         the ring-buffer upfront.
+     * 
+     * Note that samplers, textures and input attachments currently do not support array binding, since they are typically only updated once or require pipeline 
+     * synchronization anyway.
+     */
+    template <typename BufferType, typename ImageType, typename SamplerType>
+    class DescriptorSet : public IDescriptorSet
+    {
+    public:
+        using buffer_type = BufferType;
+        using image_type = ImageType;
+        using sampler_type = SamplerType;
+
+    public:
+        /// \copydoc IDescriptorSet::update()
+        virtual void update(unsigned int binding, const buffer_type& buffer, unsigned int buffer_element = 0, unsigned int elements = 0, unsigned int first_descriptor = 0) const = 0;
+
+        /// \copydoc IDescriptorSet::update()
+        virtual void update(unsigned int binding, const image_type& texture, unsigned int descriptor = 0, unsigned int first_level = 0, unsigned int levels = 0, unsigned int first_layer = 0, unsigned int layers = 0) const = 0;
+
+        /// \copydoc IDescriptorSet::update()
+        virtual void update(unsigned int binding, const sampler_type& sampler, unsigned int descriptor = 0) const = 0;
+
+        /// \copydoc IDescriptorSet::attach()
+        virtual void attach(unsigned int binding, const image_type& image) const = 0;
+
+    private:
+        void genericUpdate(unsigned binding, const IBuffer& buffer, unsigned buffer_element, unsigned elements, unsigned first_descriptor) const final { update(binding, dynamic_cast<const buffer_type&>(buffer), buffer_element, elements, first_descriptor); }
+        void genericUpdate(unsigned binding, const IImage& texture, unsigned descriptor, unsigned first_level, unsigned levels, unsigned first_layer, unsigned layers) const override { update(binding, dynamic_cast<const image_type&>(texture), descriptor, first_level, levels, first_layer, layers); }
+        void genericUpdate(unsigned binding, const ISampler& sampler, unsigned descriptor) const final { update(binding, dynamic_cast<const sampler_type&>(sampler), descriptor); }
+        void genericAttach(unsigned binding, const IImage& image) const final { attach(binding, dynamic_cast<const image_type&>(image)); }
+    };
 }
 
 template <>
