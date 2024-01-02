4dbc8050c253b992ccb772612df66e9776081e58
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-01-02 23:14:58 +0100

    spark/render: [Enh] Add GraphicsDevice class to use custom types (#27)

--- spark/render/include/spark/render/GraphicsDevice.h
+++ spark/render/include/spark/render/GraphicsDevice.h
@@ -103,4 +103,72 @@ namespace spark::render
          */
         virtual void wait() const = 0;
     };
+
+    /**
+     * \brief Represents the graphics device that a rendering back-end is doing work on.
+     * \tparam SurfaceType The type of the surface the device is rendering to. (Implements \ref ISurface)
+     * \tparam GraphicsAdapterType The type of the graphics adapter the device uses for drawing. (Implements \ref IGraphicsAdapter)
+     * \tparam SwapChainType The type of the swap chain the device uses for presentation. (Implements \ref ISwapChain)
+     * \tparam CommandQueueType The type of the command queue the device uses for draw calls. (Implements \ref ICommandQueue)
+     * \tparam RenderPassType The type of the render pass the device uses for draw calls. (Implements \ref IRenderPass)
+     *
+     * The graphics device is the central instance of a renderer.
+     * It owns the device state, which contains objects required for communication between your application and the graphics driver. Most notably, those objects
+     * contain the \ref ISwapChain instance and the \ref ICommandQueue instances used for data and command transfer.
+     */
+    template <typename FactoryType, typename SurfaceType, typename GraphicsAdapterType, typename SwapChainType, typename CommandQueueType, typename RenderPassType>
+    class GraphicsDevice : public IGraphicsDevice
+    {
+    public:
+        using surface_type = SurfaceType;
+        using adapter_type = GraphicsAdapterType;
+        using swap_chain_type = SwapChainType;
+        using command_queue_type = CommandQueueType;
+        using command_buffer_type = typename command_queue_type::command_buffer_type;
+        using factory_type = FactoryType;
+        using descriptor_layout_type = typename factory_type::descriptor_layout_type;
+        using vertex_buffer_type = typename factory_type::vertex_buffer_type;
+        using index_buffer_type = typename factory_type::index_buffer_type;
+        using buffer_type = typename factory_type::buffer_type;
+        using image_type = typename factory_type::image_type;
+        using sampler_type = typename factory_type::sampler_type;
+        using render_pass_type = RenderPassType;
+        using frame_buffer_type = typename render_pass_type::frame_buffer_type;
+        using render_pipeline_type = typename render_pass_type::render_pipeline_type;
+        using pipeline_layout_type = typename render_pipeline_type::pipeline_layout_type;
+        using shader_program_type = typename render_pipeline_type::shader_program_type;
+        using input_assembler_type = typename render_pipeline_type::input_assembler_type;
+        using rasterizer_type = typename render_pipeline_type::rasterizer_type;
+
+    public:
+        /// \copydoc IGraphicsDevice::state()
+        [[nodiscard]] DeviceState& state() noexcept override = 0;
+
+        /// \copydoc IGraphicsDevice::surface()
+        [[nodiscard]] const surface_type& surface() const noexcept override = 0;
+
+        /// \copydoc IGraphicsDevice::graphicsAdapter()
+        [[nodiscard]] const adapter_type& graphicsAdapter() const noexcept override = 0;
+
+        /// \copydoc IGraphicsDevice::swapChain()
+        [[nodiscard]] const swap_chain_type& swapChain() const noexcept override = 0;
+
+        /// \copydoc IGraphicsDevice::swapChain()
+        [[nodiscard]] swap_chain_type& swapChain() noexcept override = 0;
+
+        /// \copydoc IGraphicsDevice::factory()
+        [[nodiscard]] const factory_type& factory() const noexcept override = 0;
+
+        /// \copydoc IGraphicsDevice::graphicsQueue()
+        [[nodiscard]] const command_queue_type& graphicsQueue() const noexcept override = 0;
+
+        /// \copydoc IGraphicsDevice::transferQueue()
+        [[nodiscard]] const command_queue_type& transferQueue() const noexcept override = 0;
+
+        /// \copydoc IGraphicsDevice::bufferQueue()
+        [[nodiscard]] const command_queue_type& bufferQueue() const noexcept override = 0;
+
+        /// \copydoc IGraphicsDevice::computeQueue()
+        [[nodiscard]] const command_queue_type& computeQueue() const noexcept override = 0;
+    };
 }
