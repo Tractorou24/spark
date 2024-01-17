316ac54c097881b100e23ed17c93bb491f9b1f94
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-01-17 18:21:59 +0100

    spark/render: [Enh] Add IBackend interface (#27)

--- spark/render/src/ExportSymbols.cpp
+++ spark/render/src/ExportSymbols.cpp
@@ -1,6 +1,7 @@
 // Force the exports of symbols in the library. Include the files that are not already included in a cpp.
 // Else, the symbols are not exported and it fails to link, searching for the ctor and dtor of the interfaces.
 
+#include "spark/render/Backend.h"
 #include "spark/render/GraphicsAdapter.h"
 #include "spark/render/GraphicsDevice.h"
 #include "spark/render/Shader.h"
