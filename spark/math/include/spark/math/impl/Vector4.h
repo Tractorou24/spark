cddbb1a1ace6fc6bfa9dacaa5406c7c05d2d437a
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-02-02 17:38:38 +0100

    spark/math: [Fix] Wrong ordering of arguments in Vector4<T>
    The convention is x, y, z, w but spark was previously using w, x, y, z.

--- spark/math/include/spark/math/impl/Vector4.h
+++ spark/math/include/spark/math/impl/Vector4.h
@@ -4,22 +4,22 @@ namespace spark::math
 {
     template <typename T>
     constexpr Vector4<T>::Vector4()
-        : details::Vector<Vector4, T, 4>({std::ref(w), std::ref(x), std::ref(y), std::ref(z)}) {}
+        : details::Vector<Vector4, T, 4>({std::ref(x), std::ref(y), std::ref(z), std::ref(w)}) {}
 
     template <typename T>
-    constexpr Vector4<T>::Vector4(T value_w, T value_x, T value_y, T value_z) noexcept
-        : details::Vector<Vector4, T, 4>({std::ref(w), std::ref(x), std::ref(y), std::ref(z)}), w(std::move(value_w)),
-          x(std::move(value_x)), y(std::move(value_y)), z(std::move(value_z)) {}
+    constexpr Vector4<T>::Vector4(T value_x, T value_y, T value_z, T value_w) noexcept
+        : details::Vector<Vector4, T, 4>({std::ref(x), std::ref(y), std::ref(z), std::ref(w)}),
+          x(std::move(value_x)), y(std::move(value_y)), z(std::move(value_z)), w(std::move(value_w)) {}
 
     template <typename T>
     constexpr Vector4<T>::Vector4(const Vector4& other)
-        : details::Vector<Vector4, T, 4>({std::ref(w), std::ref(x), std::ref(y), std::ref(z)}), w(other.w), x(other.x),
-          y(other.y), z(other.z) {}
+        : details::Vector<Vector4, T, 4>({std::ref(x), std::ref(y), std::ref(z), std::ref(w)}), x(other.x), y(other.y),
+          z(other.z), w(other.w) {}
 
     template <typename T>
     constexpr Vector4<T>::Vector4(Vector4&& other) noexcept
-        : details::Vector<Vector4, T, 4>({std::ref(w), std::ref(x), std::ref(y), std::ref(z)}), w(std::move(other.w)),
-          x(std::move(other.x)), y(std::move(other.y)), z(std::move(other.z)) {}
+        : details::Vector<Vector4, T, 4>({std::ref(x), std::ref(y), std::ref(z), std::ref(w)}), x(std::move(other.x)),
+          y(std::move(other.y)), z(std::move(other.z)), w(std::move(other.w)) {}
 
     template <typename T>
     constexpr Vector4<T>& Vector4<T>::operator=(const Vector4& other)
@@ -27,10 +27,10 @@ namespace spark::math
         if (this == &other)
             return *this;
 
-        w = other.w;
         x = other.x;
         y = other.y;
         z = other.z;
+        w = other.w;
         return *this;
     }
 
@@ -40,10 +40,10 @@ namespace spark::math
         if (this == &other)
             return *this;
 
-        w = std::move(other.w);
         x = std::move(other.x);
         y = std::move(other.y);
         z = std::move(other.z);
+        w = std::move(other.w);
         return *this;
     }
 
@@ -51,6 +51,6 @@ namespace spark::math
     template <typename To>
     constexpr Vector4<To> Vector4<T>::castTo() const noexcept
     {
-        return Vector4<To>(static_cast<To>(w), static_cast<To>(x), static_cast<To>(y), static_cast<To>(z));
+        return Vector4<To>(static_cast<To>(x), static_cast<To>(y), static_cast<To>(z), static_cast<To>(w));
     }
 }
