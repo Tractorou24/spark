b3a65476df8b041c0ca2d81f5e93e827c50ec055
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-02-06 23:24:50 +0100

    spark/path: [Enh] Add a getter for the executable name (#61)

--- spark/path/src/Paths.cpp
+++ spark/path/src/Paths.cpp
@@ -6,37 +6,40 @@
 #ifdef SPARK_OS_WINDOWS
 #   include <Windows.h>
 #else
-#   include <dlfcn.h>
+#   include <unistd.h>
 #endif
+
+#include <iostream>
+
+namespace
+{
+    std::filesystem::path executable_full_path()
+    {
+#ifdef SPARK_OS_WINDOWS
+        wchar_t path[FILENAME_MAX] = {0};
+        GetModuleFileNameW(nullptr, path, FILENAME_MAX);
+        SPARK_ASSERT(std::filesystem::is_regular_file(path));
+        return path;
+#else
+        char tmp_path[FILENAME_MAX];
+        const ssize_t count = readlink("/proc/self/exe", tmp_path, FILENAME_MAX);
+        const std::filesystem::path path = std::string(tmp_path, (count > 0) ? count : 0);
+        SPARK_ASSERT(count != 0 && std::filesystem::is_regular_file(path))
+        return path;
+#endif
+    }
+}
 
 namespace spark::path
 {
     [[nodiscard]] std::filesystem::path executable_path()
     {
-#ifdef SPARK_OS_WINDOWS
-        // Get the module handle of the library containing this function.
-        HMODULE hm = nullptr;
-        const BOOL success = GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
-                                                reinterpret_cast<LPWSTR>(&executable_path),
-                                                &hm);
-        if (!success)
-            throw std::system_error(std::error_code(static_cast<int>(GetLastError()), std::system_category()), "Failed to get module handle");
+        return executable_full_path().parent_path();
+    }
 
-        // Get the full path to the executable executing this code using Win32 API.
-        WCHAR wide_path_buffer[MAX_PATH];
-        const DWORD path_size = GetModuleFileNameW(hm, wide_path_buffer, MAX_PATH);
-        if (path_size == 0 || (path_size == MAX_PATH && GetLastError() == ERROR_INSUFFICIENT_BUFFER))
-            throw std::system_error(std::error_code(static_cast<int>(GetLastError()), std::system_category()), "Failed to get module filename");
-
-        // Convert std::wstring to std::filesystem::path
-        auto result = std::filesystem::path(wide_path_buffer).parent_path();
-#else
-        Dl_info dl_info;
-        dladdr(reinterpret_cast<void*>(&executable_path), &dl_info);
-        auto result = std::filesystem::path(std::string(dl_info.dli_fname)).parent_path().parent_path().parent_path();
-#endif
-        SPARK_ASSERT(std::filesystem::is_directory(result))
-        return result;
+    std::string executable_name()
+    {
+        return executable_full_path().stem().string();
     }
 
     [[nodiscard]] std::filesystem::path assets_path()
