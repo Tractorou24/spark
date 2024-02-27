807c79bb0a6e1dcdb5b596b5e00f7662104ba664
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-02-27 17:00:51 +0100

    spark/engine/components: [Enh] Update Transform with rotation and scale (#60)
    Also adds a matrix() method to get the transform matrix for the current object.

--- spark/engine/include/spark/engine/components/Transform.h
+++ spark/engine/include/spark/engine/components/Transform.h
@@ -6,6 +6,10 @@
 
 #include "spark/math/Vector2.h"
 
+#include "glm/matrix.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/quaternion.hpp"
+
 namespace spark::engine::components
 {
     /**
@@ -16,21 +20,9 @@ namespace spark::engine::components
         DECLARE_SPARK_RTTI(Transform, Component)
 
     public:
-        math::Vector2<float> position = {};
-
-    public:
-        static math::Vector2<float> LocalToWorld(const Transform* object)
-        {
-            math::Vector2<float> position = {};
-
-            auto* game_object = object->gameObject();
-            while (game_object->parent() != nullptr)
-            {
-                position += game_object->transform()->position;
-                game_object = game_object->parent();
-            }
-            return position;
-        }
+        math::Vector2<float> position = {0.f, 0.f};
+        float rotation = 0;
+        math::Vector2<float> scale = {1.f, 1.f};
 
     public:
         explicit Transform(GameObject* parent)
@@ -38,6 +30,33 @@ namespace spark::engine::components
 
         friend bool operator==(const Transform& lhs, const Transform& rhs) { return lhs.position == rhs.position; }
         friend bool operator!=(const Transform& lhs, const Transform& rhs) { return !(lhs == rhs); }
+
+        /**
+         * \brief Gets the transformation matrix of the transform.
+         * \return A \ref glm::mat4 representing the transformation matrix.
+         */
+        glm::mat4 matrix() const
+        {
+            glm::mat4 matrix(1.f);
+
+            // Apply local transform
+            matrix = glm::translate(matrix, {gameObject()->transform()->position.x, gameObject()->transform()->position.y, 0.f});
+            matrix = glm::rotate(matrix, gameObject()->transform()->rotation, {0.0f, 0.0f, 1.0f});
+            matrix = glm::scale(matrix, {gameObject()->transform()->scale.x, gameObject()->transform()->scale.y, 1.0f});
+
+            // Apply parent transforms
+            const GameObject* parent = gameObject()->parent();
+            while (parent)
+            {
+                const Transform& parent_transform = *parent->transform();
+                matrix = glm::translate(matrix, {parent_transform.position.x, parent_transform.position.y, 0.f});
+                matrix = glm::rotate(matrix, parent_transform.rotation, {0.0f, 0.0f, 1.0f});
+                matrix = glm::scale(matrix, {parent_transform.scale.x, parent_transform.scale.y, 1.0f});
+                parent = parent_transform.gameObject()->parent();
+            }
+
+            return matrix;
+        }
     };
 }
 
