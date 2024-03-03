b76f9f415feaf4f5e4fdac7f65eb30462a7631c4
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-03-03 19:49:30 +0100

    spark/core: [Enh] Add a registry for components (#70)

--- spark/core/include/spark/core/impl/Registries.h
+++ spark/core/include/spark/core/impl/Registries.h
@@ -8,4 +8,11 @@ namespace spark::core
         using Parent = patterns::Factory<std::string, core::GameObject, std::string, core::GameObject*>;
         Parent::registerType<T>(T::classRtti().className());
     }
+
+    template <typename T> requires std::derived_from<T, core::Component>
+    void ComponentRegistry::registerType()
+    {
+        using Parent = patterns::Factory<std::string, core::Component, core::GameObject*>;
+        Parent::registerType<T>(T::classRtti().className());
+    }
 }
