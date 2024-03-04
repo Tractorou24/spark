804cb7f0806f90f3951522fcdcc4dc63ac9e7664
Lussiez Stephen <152852759+LeonLighter@users.noreply.github.com>
2024-03-04 14:00:46 +0100

    examples/brickbreaker: [Enh] Add bricks (#62)
    Now, we have all the destroyable bricks on the scene.

--- examples/brickbreaker/src/main.cpp
+++ examples/brickbreaker/src/main.cpp
@@ -1,4 +1,5 @@
 #include "brickbreaker/Ball.h"
+#include "brickbreaker/Brick.h"
 #include "brickbreaker/Paddle.h"
 #include "brickbreaker/ScreenBorder.h"
 
@@ -11,7 +12,11 @@
 
 std::unique_ptr<spark::core::Application> spark_main(const std::vector<std::string>& /*args*/)
 {
-    auto app = spark::core::ApplicationBuilder().setName("BrickBreaker").setSize(1280, 720).build();
+    static constexpr spark::math::Vector2 window_size = {1280u, 720u};
+    static constexpr spark::math::Vector2 brick_count = {7u, 10u};
+    static constexpr spark::math::Vector2 brick_size = {100.f, 40.f};
+
+    auto app = spark::core::ApplicationBuilder().setName("BrickBreaker").setSize(window_size.x, window_size.y).build();
 
     {
         auto* root = spark::core::GameObject::Instantiate("Scene Root", nullptr);
@@ -24,6 +29,16 @@ std::unique_ptr<spark::core::Application> spark_main(const std::vector<std::stri
         // Add the game objects
         spark::core::GameObject::Instantiate<brickbreaker::Paddle>("Paddle", root, spark::math::Vector2<float> {150, 25});
         spark::core::GameObject::Instantiate<brickbreaker::Ball>("Ball", root, 15.0f);
+
+        auto* brick_container = spark::core::GameObject::Instantiate<spark::core::GameObject>("Brick Container", root);
+        for (unsigned row = 0; row < brick_count.x; row++)
+        {
+            for (unsigned column = 0; column < brick_count.y; column++)
+            {
+                const auto* brick = spark::core::GameObject::Instantiate<brickbreaker::Brick>(std::format("Brick{}{}", row, column), brick_container, brick_size);
+                brick->transform()->position = spark::math::Vector2(window_size.x / 8 + column * brick_size.x + column * 2, window_size.y / 8 + row * brick_size.y + row * 2);
+            }
+        }
 
         // Register the main game scene
         auto scene = std::make_unique<spark::core::Scene>(root);
