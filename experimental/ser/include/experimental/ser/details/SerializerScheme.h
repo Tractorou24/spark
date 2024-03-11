4cb326ae22eef407fc52c1b3f58f517e57d41ab2
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-03-11 14:50:44 +0100

    experimental/ser: [Fix] Compile error when using serializer rtti without new fields (#70)
    Problem:
    The program does not compiles when using
    SPARK_SER_DETAILS_SERIALIZE_SIMPLE_CLASS without any field arguments.
    
    Cause:
    The macro expansion produces an 'obj.' even if there is no object to
    precess.
    
    Solution:
    Check if the variadic list is empty before expanding the macro.

--- experimental/ser/include/experimental/ser/details/SerializerScheme.h
+++ experimental/ser/include/experimental/ser/details/SerializerScheme.h
@@ -3,6 +3,7 @@
 #include "spark/mpl/index_list.h"
 #include "spark/rtti/HasRtti.h"
 
+#include "boost/preprocessor/empty.hpp"
 #include "boost/preprocessor/seq/for_each.hpp"
 
 namespace experimental::ser::details
@@ -25,26 +26,26 @@ namespace experimental::ser::details
         }                                                                                                   \
     };
 
-#define SPARK_SER_DETAILS_SERIALIZE_RTTI_CLASS(ClassName, ...)                                                                  \
-    template<typename SerializerType>                                                                                           \
-    struct experimental::ser::SerializerScheme<SerializerType, ClassName>                                                       \
-    {                                                                                                                           \
-        static_assert(std::is_base_of_v<spark::rtti::HasRtti, ClassName>, "The class must be registered into the RTTI.");       \
-                                                                                                                                \
-        static void serialize(SerializerType& serializer, const ClassName& obj)                                                 \
-        {                                                                                                                       \
-            if constexpr (ClassName::ThisRttiType::ParentTypes::size > 0)                                                       \
-                spark::mpl::for_sequence(std::make_index_sequence<ClassName::ThisRttiType::ParentTypes::size> {},               \
-                         [&](auto i) { serializer << static_cast<const ClassName::ThisRttiType::ParentTypes::at<i>&>(obj); });  \
-            BOOST_PP_SEQ_FOR_EACH(SPARK_SER_DETAILS_SERIALIZE, _, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))                        \
-        }                                                                                                                       \
-                                                                                                                                \
-        static void deserialize(SerializerType& serializer, ClassName& obj)                                                     \
-        {                                                                                                                       \
-            if constexpr (ClassName::ThisRttiType::ParentTypes::size > 0)                                                       \
-                spark::mpl::for_sequence(std::make_index_sequence<ClassName::ThisRttiType::ParentTypes::size> {},               \
-                         [&](auto i) { serializer >> static_cast<ClassName::ThisRttiType::ParentTypes::at<i>&>(obj); });        \
-            BOOST_PP_SEQ_FOR_EACH(SPARK_SER_DETAILS_DESERIALIZE, _, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))                      \
-        }                                                                                                                       \
+#define SPARK_SER_DETAILS_SERIALIZE_RTTI_CLASS(ClassName, ...)                                                                                                                  \
+    template<typename SerializerType>                                                                                                                                           \
+    struct experimental::ser::SerializerScheme<SerializerType, ClassName>                                                                                                       \
+    {                                                                                                                                                                           \
+        static_assert(std::is_base_of_v<spark::rtti::HasRtti, ClassName>, "The class must be registered into the RTTI.");                                                       \
+                                                                                                                                                                                \
+        static void serialize(SerializerType& serializer, const ClassName& obj)                                                                                                 \
+        {                                                                                                                                                                       \
+            if constexpr (ClassName::ThisRttiType::ParentTypes::size > 0)                                                                                                       \
+                spark::mpl::for_sequence(std::make_index_sequence<ClassName::ThisRttiType::ParentTypes::size> {},                                                               \
+                         [&](auto i) { serializer << static_cast<const ClassName::ThisRttiType::ParentTypes::at<i>&>(obj); });                                                  \
+            BOOST_PP_IF(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), BOOST_PP_SEQ_FOR_EACH(SPARK_SER_DETAILS_SERIALIZE, _, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)), BOOST_PP_EMPTY())    \
+        }                                                                                                                                                                       \
+                                                                                                                                                                                \
+        static void deserialize(SerializerType& serializer, ClassName& obj)                                                                                                     \
+        {                                                                                                                                                                       \
+            if constexpr (ClassName::ThisRttiType::ParentTypes::size > 0)                                                                                                       \
+                spark::mpl::for_sequence(std::make_index_sequence<ClassName::ThisRttiType::ParentTypes::size> {},                                                               \
+                         [&](auto i) { serializer >> static_cast<ClassName::ThisRttiType::ParentTypes::at<i>&>(obj); });                                                        \
+            BOOST_PP_IF(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), BOOST_PP_SEQ_FOR_EACH(SPARK_SER_DETAILS_DESERIALIZE, _, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)), BOOST_PP_EMPTY())  \
+        }                                                                                                                                                                       \
     };
 }
