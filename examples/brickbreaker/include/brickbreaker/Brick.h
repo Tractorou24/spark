0fc438ab71ccebfea466d96b482f58c8dce5c25f
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2024-03-22 23:35:46 +0100

    examples/brickbreaker: [Enh] Randomize brick color (#62)

--- examples/brickbreaker/include/brickbreaker/Brick.h
+++ examples/brickbreaker/include/brickbreaker/Brick.h
@@ -4,6 +4,7 @@
 #include "spark/core/GameObject.h"
 #include "spark/core/components/Collider.h"
 #include "spark/core/components/Rectangle.h"
+#include "spark/lib/Random.h"
 #include "spark/rtti/HasRtti.h"
 
 namespace brickbreaker
@@ -21,9 +22,32 @@ namespace brickbreaker
         {
             addComponent<spark::core::components::Rectangle>();
             component<spark::core::components::Rectangle>()->size = size;
+            component<spark::core::components::Rectangle>()->color = spark::lib::Random::ElementInRange(s_allowedColors);
 
             addComponent<spark::core::components::StaticCollider>(spark::math::Rectangle({0, 0}, size));
         }
+
+    private:
+        inline static constexpr std::array<spark::math::Vector4<float>, 16> s_allowedColors = {
+            {
+                {1.0f, 0.0f, 0.0f, 1.0f}, // Red
+                {0.0f, 1.0f, 0.0f, 1.0f}, // Green
+                {0.0f, 0.0f, 1.0f, 1.0f}, // Blue
+                {1.0f, 1.0f, 0.0f, 1.0f}, // Yellow
+                {0.0f, 1.0f, 1.0f, 1.0f}, // Cyan
+                {1.0f, 0.0f, 1.0f, 1.0f}, // Magenta
+                {0.5f, 0.5f, 0.5f, 1.0f}, // Gray
+                {1.0f, 0.5f, 0.0f, 1.0f}, // Orange
+                {0.5f, 0.0f, 1.0f, 1.0f}, // Purple
+                {0.0f, 0.5f, 1.0f, 1.0f}, // Light Blue
+                {0.5f, 1.0f, 0.0f, 1.0f}, // Lime
+                {1.0f, 0.0f, 0.5f, 1.0f}, // Pink
+                {0.0f, 1.0f, 0.5f, 1.0f}, // Sea Green
+                {0.5f, 0.0f, 0.5f, 1.0f}, // Dark Purple
+                {0.0f, 0.5f, 0.5f, 1.0f}, // Teal
+                {0.5f, 0.5f, 0.0f, 1.0f}, // Olive
+            }
+        };
     };
 }
 
