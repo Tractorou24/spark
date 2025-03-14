900b03f6fbc01cfe5b0cbb9a719455735c1aa7e4
Fabian INGREMEAU <fabian.ingremeau@outlook.com>
2025-03-14 23:42:34 +0100

    example/boids: [Enh] Add a BoidsManager with a number of boids and a cell system
    When launhing, we now have a UI which allows to configure the number of boids.
    The window is divided into 50px cells and the manager knows at each time where
    all boids are. They are stored in map by ID for easy access.

--- examples/boids/include/boids/Bird.h
+++ examples/boids/include/boids/Bird.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "spark/core/GameObject.h"
+#include "spark/core/Input.h"
 #include "spark/core/components/Rectangle.h"
 #include "spark/core/components/Transform.h"
 
@@ -14,11 +15,48 @@ namespace boids
         DECLARE_SPARK_RTTI(Bird, GameObject)
 
     public:
-        explicit Bird(std::string name, spark::core::GameObject* parent)
+        spark::patterns::Signal<Bird*, std::size_t> onCellChanged;
+
+    public:
+        explicit Bird(std::string name, spark::core::GameObject* parent, spark::math::Vector2<float> position)
             : spark::core::GameObject(std::move(name), parent)
         {
             addComponent<spark::core::components::Rectangle>(spark::math::Vector2<float> {15, 15.f}, spark::math::Vector4<float> {1, 1, 1, 1});
+            transform()->position = std::move(position);
+            m_currentCellId = cell();
         }
+
+        /**
+         * \brief Gets the cell unique ID the bird is in.
+         * \return The cell unique ID.
+         */
+        [[nodiscard]] std::size_t cell() const
+        {
+            const auto position = transform()->position.castTo<std::size_t>();
+            return position.x / 50 * 100 + position.y / 50;
+        }
+
+        void onUpdate(const float dt) override
+        {
+            const auto mouse_position = spark::core::Input::MousePosition();
+            const auto direction = (mouse_position - transform()->position).normalized();
+
+            transform()->position += direction * 100.f * dt;
+
+            // Update the cell position if it changed
+            if (const auto new_cell = cell(); m_currentCellId != new_cell)
+            {
+                auto old_cell = m_currentCellId;
+                m_currentCellId = new_cell;
+                onCellChanged.emit(this, std::move(old_cell));
+            }
+        }
+
+        friend bool operator==(const Bird& lhs, const Bird& rhs) { return lhs.uuid() == rhs.uuid(); }
+        friend bool operator!=(const Bird& lhs, const Bird& rhs) { return !(lhs == rhs); }
+
+    private:
+        std::size_t m_currentCellId = 0;
     };
 }
 
